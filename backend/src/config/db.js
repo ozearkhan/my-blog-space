ef09fd21ea768310e2796d591ccc7cf5230b7868
Ozear Khan <ozearkhan1224@gmail.com>
2024-10-20 00:42:12 +0530

    migrated database schema and generated clients

---
+++ backend/src/config/db.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+export { prisma }
