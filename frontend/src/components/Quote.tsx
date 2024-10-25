444e0dd46204b431f39eacdc120b5ac58013304c
Ozear Khan <ozearkhan1224@gmail.com>
2024-10-25 23:54:57 +0530

    added auth component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+
+export const Quote = ()=>{
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg text-3xl font-bold">
+                <div className="text-3xl font-bold">
+                    "The customer support I received was exceptional. The support team went above and beyond to address
+                    my concern"
+                </div>
+                <div className="max-w-lg text-xl font-semibold pt-5">
+                    Julies Winfield
+                </div>
+                <div className="max-w-lg text-sm font-light text-slate-400">
+                    CEO| MyCorp
+                </div>
+            </div>
+        </div>
+
+    </div>
+}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,16 @@
+import {Quote} from "../components/Quote.tsx";
+import {Auth} from "../components/Auth.tsx";
+
 export const Signup = ()=>{
-    return <>
-        Signup Page
-    </>
+    return <div>
+        <div className="grid grid-cols-2">
+            <div>
+                <Auth type="signup"/>
+            </div>
+            <div className="invisible lg:visible">
+                <Quote />
+            </div>
+
+        </div>
+    </div>
 }
