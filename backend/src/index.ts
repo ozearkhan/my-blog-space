de671ca3f9c4faeebe519af4c5fa3f4a495ebf44
Ozear Khan <ozearkhan1224@gmail.com>
2024-10-26 20:56:42 +0530

    completed basic frontend of the project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono'
 import { server } from './routes/routes'
 const app = new Hono()
+import { cors } from 'hono/cors'
 
-
+app.use('/',cors())
 app.route('/',server)
 
 export default app
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { blog } from "../domains/blog/"
+import { blog } from '../domains/blog/'
 import { user } from '../domains/user'
 
 export const server = new Hono().basePath('api/v1')
